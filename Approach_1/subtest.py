7bd5623be068ad64c6863abc99e8bce9d471242e
AttilaNightStalker <attilawang1226@gmail.com>
2020-02-27 07:08:53 +0000

    fancy 3d graph

--- Approach_1/subtest.py
+++ Approach_1/subtest.py
@@ -1,7 +1,26 @@
-from subscriber import Subscriber
 
-my_sub = Subscriber("10.0.0.1", 5557)
-my_sub.start()
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
 
-my_sub.register_sub("topic1")
-my_sub.register_sub("topic2")
+def plotResult():
+	with open("record.txt", "r") as fp:
+		data = list(map(lambda x: x.split(","), filter(lambda x: not x == "", fp.read().split("\n"))))
+
+	P = np.array([eval(d[0]) for d in data])
+	S = np.array([eval(d[1]) for d in data])
+	t = np.array([eval(d[2]) for d in data])
+
+	print(P)
+	print(S)
+	print(t)
+
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+	ax.plot_trisurf(P, S, t, linewidth=0, antialiased=True)
+	ax.set_xlabel('publisher number')
+	ax.set_ylabel('subscriber number')
+	ax.set_zlabel('average delay');
+	plt.show()
+
+plotResult()
